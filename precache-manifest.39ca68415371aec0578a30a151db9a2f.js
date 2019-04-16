73aa0500fdf77ed62319c00a3e19e4a69092162d
Trevor Wood <trevorwood222@gmail.com>
2019-04-16 18:52:21 +0900

    Updates

---
+++ precache-manifest.39ca68415371aec0578a30a151db9a2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "557fcc0f5c26ddea3af5d22857595b2c",
+    "url": "/pokemongoxp/static/media/pokemon-go-logo-transparent.557fcc0f.png"
+  },
+  {
+    "revision": "d082b5f54598b4773eae",
+    "url": "/pokemongoxp/static/js/runtime~main.5b9ce9e0.js"
+  },
+  {
+    "revision": "18cb1fc0dcb0e41ba3b7",
+    "url": "/pokemongoxp/static/js/main.9feadec9.chunk.js"
+  },
+  {
+    "revision": "525b4a0f5fb254abd410",
+    "url": "/pokemongoxp/static/js/2.c65786f7.chunk.js"
+  },
+  {
+    "revision": "18cb1fc0dcb0e41ba3b7",
+    "url": "/pokemongoxp/static/css/main.a163a3e3.chunk.css"
+  },
+  {
+    "revision": "ff2b193e5775768bcc3174661ad5c360",
+    "url": "/pokemongoxp/index.html"
+  }
+];
