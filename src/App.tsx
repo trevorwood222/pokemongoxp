eabcf21575b2e7337daa287e2177287bad918528
Trevor Wood <trevorwood222@gmail.com>
2019-04-17 15:35:28 +0900

    Update package

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.scss';
 import i18n from './i18n/i18n';
 
-import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
+import { BrowserRouter,Route, Switch } from 'react-router-dom'
 import Calculator from './components/Calculator/Calculator'
 import Result from './components/Result/Result';
 import RawData from './components/RawData/RawData';
@@ -14,15 +14,18 @@ export default class App extends Component {
     document.documentElement.lang = i18n.t('lang');
 
     return (
-      <div className="app">
-        <Switch>
-          <Route exact path={"/"} component={Calculator} />
-          <Route path={"/result"} component={Result} />
-          <Route path={"/rawdata"} component={RawData} />
-          <Route path={"/raw-data"} component={RawData} />
-          <Route component={NotFound} />
-        </Switch>
-      </div>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <div className="app">
+          <Switch>
+            <Route exact path={"/"} component={Calculator} />
+            <Route path={"/result"} component={Result} />
+            <Route path={"/rawdata"} component={RawData} />
+            <Route path={"/raw-data"} component={RawData} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+
     );
   }
 }
