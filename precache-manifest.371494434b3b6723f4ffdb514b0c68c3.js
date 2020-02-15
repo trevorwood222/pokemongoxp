689104876511f3c520939aaf5fbb192c2e90d183
Trevor Wood <trevorwood222@gmail.com>
2020-02-15 12:43:45 +0900

    Updates

---
+++ precache-manifest.371494434b3b6723f4ffdb514b0c68c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8587d4899d95648ce211bab42b0057f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e65cc043cbf09c96305",
+    "url": "/static/css/main.e187336d.chunk.css"
+  },
+  {
+    "revision": "66738ffb923c6732fd9b",
+    "url": "/static/js/2.f9840935.chunk.js"
+  },
+  {
+    "revision": "507c4436aa55e48136618e82a79cbf78",
+    "url": "/static/js/2.f9840935.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e65cc043cbf09c96305",
+    "url": "/static/js/main.e2c689cc.chunk.js"
+  },
+  {
+    "revision": "3e262e29c5498fb031ca",
+    "url": "/static/js/runtime-main.362888ef.js"
+  },
+  {
+    "revision": "557fcc0f5c26ddea3af5d22857595b2c",
+    "url": "/static/media/pokemon-go-logo-transparent.557fcc0f.png"
+  }
+]);
