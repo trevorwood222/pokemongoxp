df74057dce5155bcf811c24b5a81c63809d3b85b
Trevor Wood <trevorwood222@gmail.com>
2020-04-03 11:01:51 +0900

    Updates

---
+++ precache-manifest.52bbea97a298f62f98aad17130aebb2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d497ffd11da58c716d93071a2d2acf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d05dced65acaa16cde25",
+    "url": "/static/css/main.37d2f3b7.chunk.css"
+  },
+  {
+    "revision": "a1a502a48d020bec8683",
+    "url": "/static/js/2.3044f110.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3044f110.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d05dced65acaa16cde25",
+    "url": "/static/js/main.3da5c3dc.chunk.js"
+  },
+  {
+    "revision": "70a00ab4fe503e529921",
+    "url": "/static/js/runtime-main.38699a88.js"
+  },
+  {
+    "revision": "557fcc0f5c26ddea3af5d22857595b2c",
+    "url": "/static/media/pokemon-go-logo-transparent.557fcc0f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.371494434b3b6723f4ffdb514b0c68c3.js"
+  "/precache-manifest.52bbea97a298f62f98aad17130aebb2d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
